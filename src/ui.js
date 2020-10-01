351032206ce2d06c800da437c72d3a350f02b33f
Yash Arora <arorayash.work@gmail.com>
2020-10-01 23:21:59 +0530

    maintenence cleanup

--- src/ui.js
+++ src/ui.js
@@ -1,29 +1,40 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './ui.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./ui.css";
 class App extends React.Component {
-    constructor() {
-        super(...arguments);
-        this.countRef = (element) => {
-            if (element)
-                element.value = '5';
-            this.textbox = element;
-        };
-        this.onCreate = () => {
-            const count = parseInt(this.textbox.value, 10);
-            parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
-        };
-        this.onCancel = () => {
-            parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
-        };
-        this.onSearch = (event) => {
-            console.log(event.target.value);
-            parent.postMessage({ pluginMessage: { type: 'search', query: event.target.value } }, '*');
-        };
-    }
-    render() {
-        return React.createElement("div", null,
-            React.createElement("input", { onChange: this.onSearch }));
-    }
+  constructor() {
+    super(...arguments);
+    this.countRef = (element) => {
+      if (element) element.value = "5";
+      this.textbox = element;
+    };
+    this.onCreate = () => {
+      const count = parseInt(this.textbox.value, 10);
+      parent.postMessage(
+        { pluginMessage: { type: "create-rectangles", count } },
+        "*"
+      );
+    };
+    this.onCancel = () => {
+      parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+    };
+    this.onSearch = (event) => {
+      console.log(event.target.value);
+      parent.postMessage(
+        { pluginMessage: { type: "search", query: event.target.value } },
+        "*"
+      );
+    };
+  }
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("input", { onChange: this.onSearch })
+    );
+  }
 }
-ReactDOM.render(React.createElement(App, null), document.getElementById('react-page'));
+ReactDOM.render(
+  React.createElement(App, null),
+  document.getElementById("react-page")
+);
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,41 +1,44 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './ui.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./ui.css";
 
-declare function require(path: string): any
+declare function require(path: string): any;
 
 class App extends React.Component {
-  textbox: HTMLInputElement
+  textbox: HTMLInputElement;
 
   countRef = (element: HTMLInputElement) => {
-    if (element) element.value = '5'
-    this.textbox = element
-  }
+    if (element) element.value = "5";
+    this.textbox = element;
+  };
 
   onCreate = () => {
-    const count = parseInt(this.textbox.value, 10)
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-  }
+    const count = parseInt(this.textbox.value, 10);
+    parent.postMessage(
+      { pluginMessage: { type: "create-rectangles", count } },
+      "*"
+    );
+  };
 
   onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-  }
+    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  };
 
   onSearch = (event) => {
-    console.log(event.target.value)
-    parent.postMessage({ pluginMessage: { type: 'search', query: event.target.value } }, '*')
-  }
+    console.log(event.target.value);
+    parent.postMessage(
+      { pluginMessage: { type: "search", query: event.target.value } },
+      "*"
+    );
+  };
 
   render() {
-    return <div>
-      {/* <img src={require('./logo.svg')} /> */}
-      <input onChange={this.onSearch}/>
-      {/* <h2>Rectangle Creator</h2>
-      <p>Count: <input ref={this.countRef} /></p>
-      <button id="create" onClick={this.onCreate}>Create</button>
-      <button onClick={this.onCancel}>Cancel</button> */}
-    </div>
+    return (
+      <div>
+        <input onChange={this.onSearch} />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('react-page'))
+ReactDOM.render(<App />, document.getElementById("react-page"));
